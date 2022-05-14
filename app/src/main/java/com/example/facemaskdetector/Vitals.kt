939b9f919eb1020385ed1a5dc0fab6762368bb8a
Conceited99 <60193506+Conceited99@users.noreply.github.com>
2022-05-13 19:20:29 -0500

    vitals percentage countdown

--- app/src/main/java/com/example/facemaskdetector/Vitals.kt
+++ app/src/main/java/com/example/facemaskdetector/Vitals.kt
@@ -108,7 +108,7 @@ class Vitals : AppCompatActivity() {
                                 {
                                     Butt.setText("80%");
                                 }
-                                else if(i>24 && i<=28)
+                                else if(i>24 && i<=29)
                                 {
                                     Butt.setText("90%");
                                 }
